c4d36606eab6443eb90d6595da7c48505e2a514b
EdgarAleG <edgargonzalezalejandro0@gmail.com>
2024-08-05 19:54:26 -0300

    first part of the aplication, only backend

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+const generateId = () => {
+  const maxId = notes.length > 0 
+    ? Math.max(...notes.map(n => n.id))
+    : 0;
+  return maxId + 1;
+}
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World en puerto 3001 probando</h1>");
+});
+
+app.get("/api/notes", (resquest, response) => {
+  response.json(notes);
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((item) => item.id === id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.statusMessage = "page not found";
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter(item => item.id !== id)
+  response.status(204).end();
+})
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if(!note.content) {
+    return response.status(400).json({
+      error: "content missing"
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note);
+
+  console.log(note);
+  response.json(note);
+})
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server runing on port ${PORT}`);
+});
